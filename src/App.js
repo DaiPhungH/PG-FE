5c79ae8f417308c3ff799f7c8690a36b13aaf82d
Đại Phùng Hữu <daiphung79@gmail.com>
2024-08-20 19:04:00 +0700

    <message>

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HeaderComponent from './Header/Header';
+import BodyComponent from './Body/Body';
+import Footer from './Footer/Footer';
+function App() {
+  return (
+    <div className="App">
+      <HeaderComponent/>
+      <BodyComponent/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Body/Body.js
@@ -0,0 +1,10 @@
+
+import "bootstrap/dist/css/bootstrap.css";
+
+const BodyComponent = () => {
+  return (
+    <div><h1>This is body</h1></div>
+  );
+};
+
+export default BodyComponent;
